99a0f493ca668e70175284237c8b677735abb306
tli2-mac <tli2@xiaohongshu.com>
2018-07-16 21:26:20 +0800

    fix

--- apps/recode_lambda.py
+++ apps/recode_lambda.py
@@ -17,26 +17,17 @@ with open(file_path + '/tmp/train.conf', 'r') as f:
         print("key %s , value %s" % (terms[0], terms[2]))
 
 
-# # load or create your dataset
-# print('Load data...')
-# df_train = pd.read_csv('regression.train', header=None, sep='\t')
-# df_test = pd.read_csv('regression.test', header=None, sep='\t')
+# load or create your dataset
+print('Load data...')
+td_train = pd.read_csv('file_path/tmp/train_data', header=None, sep=',').values
+td_query = pd.read_csv('file_path/tmp/train_data.query', header=None, sep=',').values
+vd_train = pd.read_csv('file_path/tmp/vali_data', header=None, sep=',').values
+vd_query = pd.read_csv('file_path/tmp/vali_data.query', header=None, sep=',').values
 
-# y_train = df_train[0].values
-# y_test = df_test[0].values
-# X_train = df_train.drop(0, axis=1).values
-# X_test = df_test.drop(0, axis=1).values
 
-# # create dataset for lightgbm
-# lgb_train = lgb.Dataset(X_train, y_train)
-# lgb_test = lgb.Dataset(X_test, y_test, reference=lgb_train)
-
-# # specify your configurations as a dict
-# params = {
-#     'num_leaves': 5,
-#     'metric': ('l1', 'l2'),
-#     'verbose': 0
-# }
+# create dataset for lightgbm
+lgb_train = lgb.Dataset(td_train, group=td_query)
+lgb_test = lgb.Dataset(vd_train, group=vd_query, reference=lgb_train)
 
 # evals_result = {}  # to record eval results for plotting
 
